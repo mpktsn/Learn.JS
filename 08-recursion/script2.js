53733c39d4352672d84cab3ada49b157d5abc401
mpktsn <mpktsn@users.noreply.github.com>
2017-02-16 17:55:03 +0300

    new assignment - factorial calculation

---
+++ 08-recursion/script2.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/* 
+
+Compute the factorial.
+
+Write the factorial computing function with recursion.
+It must return the factorial of n!.
+
+*/
+
+function factorial(n) {
+    if (n == 0) return 1;
+    if (n == 1) return 1;
+    for (var i = n; i = 1; i--) {
+        return n * factorial (n - 1);
+    }
+}
+
+var n = Number(prompt("Enter number: ", ''));
+
+alert(factorial(n));
